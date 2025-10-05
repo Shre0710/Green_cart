354e7567c17560896be4ff925cc13acdfb4b3815
Shre0710 <shreyabpatil04@gmail.com>
2025-10-05 12:45:49 +0530

    Added seller pages and updated assets/context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,18 +11,28 @@ import AllProducts from './pages/AllProducts'
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
+import AddAddress from './pages/AddAddress'
+import MyOrders from './pages/MyOrders'
+import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
+
+
+
 
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const { showUserLogin, isSeller } = useAppContext()
 
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
 
       {isSellerPath ? null : <Navbar />}
-      {showUserLogin? <Login /> : null}
+      {showUserLogin ? <Login /> : null}
 
 
 
@@ -36,8 +46,16 @@ const App = () => {
           <Route path='/products/:category' element={<ProductCategory />} />
           <Route path='/products/:category/:id' element={<ProductDetails />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/add-address' element={<AddAddress />} />
+          <Route path='/my-orders' element={<MyOrders />} />
+          <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
+
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
 
 
+          </Route>
 
 
 
@@ -45,7 +63,7 @@ const App = () => {
         </Routes>
 
       </div>
-      { !isSellerPath && < Footer />}
+      {!isSellerPath && < Footer />}
     </div>
   )
 }
---
+++ client/src/pages/seller/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Orders</h1>
+    </div>
+  );
+};
+
+export default Orders;
